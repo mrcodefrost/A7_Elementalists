18180f7217ad68d403242d3c889e22c577f65069
mrcodefrost <72447688+mrcodefrost@users.noreply.github.com>
2021-02-07 11:00:27 +0530

    Update score.dart

--- lib/screens/score.dart
+++ lib/screens/score.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:womania/const.dart';
 import 'package:womania/controllers/questions_ctrl.dart';
 
 class ScoreScreen extends StatelessWidget {
@@ -16,20 +15,31 @@ class ScoreScreen extends StatelessWidget {
             children: [
               Spacer(flex: 3),
               Text(
-                "Score",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline3
-                    .copyWith(color: kSecondaryColor),
+                "Congratulations !!",
+                style: TextStyle(
+                  fontFamily: 'Dancing Script',
+                  color: Colors.green,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Spacer(),
+              Text("You Scored",
+                  style: TextStyle(
+                    fontFamily: 'Cinzel',
+                    color: Colors.green,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Spacer(),
               Text(
-                "${_qnControl.numOfCorrectAns * 10}/${_qnControl.questions.length * 10}",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline4
-                    .copyWith(color: kSecondaryColor),
-              ),
+                  "${_qnControl.numOfCorrectAns * 10}/${_qnControl.questions.length * 10}",
+                  style: TextStyle(
+                    fontFamily: 'Fredericka the Great',
+                    color: Colors.greenAccent,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  )),
               Spacer(flex: 3),
             ],
           )
